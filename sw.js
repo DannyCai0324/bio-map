7b1651309ce4d530659fd3790fe72ea63cc7d006
Chang-Yi Tsai <141116858+DannyCai0324@users.noreply.github.com>
2026-08-07 09:55:26 +0800

    補上選修Ⅰ 4-3 基因表現的調控講義＋6 題；插圖改由獨立的 figures.json 載入

--- sw.js
+++ sw.js
@@ -1,20 +1,16 @@
-const CACHE = 'biomap-89763da4';
+const CACHE = 'biomap-588c03e8';
 const ASSETS = ['./', './index.html', './manifest.webmanifest',
-                './icon-192.png', './icon-512.png'];
+                './icon-192.png', './icon-512.png', './figures.json'];
 
 self.addEventListener('install', e => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {}));
 });
-
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(ks => Promise.all(
-      ks.filter(k => k !== CACHE).map(k => caches.delete(k))
-    )).then(() => self.clients.claim())
-  );
+  e.waitUntil(caches.keys().then(ks => Promise.all(
+    ks.filter(k => k !== CACHE).map(k => caches.delete(k))
+  )).then(() => self.clients.claim()));
 });
-
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return;
   e.respondWith(
